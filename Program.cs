e29d22246c90f841afa7aac5272888b65a5fbded
Daniil <rychenkov16@yandex.ru>
2024-08-04 16:40:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace MyProject5_6
+{
+    class Program
+    {
+        static (string Name, string LastName, int Age) User_info()
+        {
+            (string Name, string LastName, int Age) User;
+
+            Console.WriteLine("Введите имя: ");
+            User.Name = Console.ReadLine();
+
+            Console.WriteLine("Введите фамилию: ");
+            User.LastName = Console.ReadLine();
+
+
+            string age;
+            int intage;
+            do
+            {
+                Console.WriteLine("Введите возраст: ");
+                age = Console.ReadLine();
+            }
+            while (CheckNum(age, out intage));
+            User.Age = intage;
+
+            return User;
+
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
